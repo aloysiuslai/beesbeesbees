bda4c7580580e2df58e7d5bda1d691bc37101961
aloysiuslai <aloysiuslai13@gmail.com>
2020-03-30 11:21:43 -0700

    Pass Bees Bees Bees Test

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,13 @@
 var Bee = function() {
+  //this = Object.create(Bee.prototype);
+  Grub.call(this);
+
+  this.age = 5;
+  this.color = "yellow";
+  this.job = "keep on growing";
+
+  // re turn this;
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
